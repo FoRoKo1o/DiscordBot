093c5f5390f6bace27b9d119b57997e1f1198830
FoRoKo1o <93284844+FoRoKo1o@users.noreply.github.com>
2023-02-26 08:48:56 +0100

    Delete commands/testCommands directory

--- commands/testCommands/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'test',
-  description: 'testowa komenda',
-  usage: 'test',
-  execute: async(message, args) => {
-    console.log('wyperdalaj');
-    message.channel.send('test') ;
-  }
-}
